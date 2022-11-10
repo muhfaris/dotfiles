b8d778e83dcf0d0ba6f390d5251cc5a2b312d01f
Muhammad Faris <muhfaris@disroot.org>
2022-11-10 08:49:45 +0700

    fix: tab map

--- neovim/configuration/lua/tabby.lua
+++ neovim/configuration/lua/tabby.lua
@@ -45,3 +45,13 @@ require("tabby.tabline").set(function(line)
 		hl = theme.fill,
 	}
 end)
+
+vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+-- move current tab to previous position
+vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+-- move current tab to next position
+vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
